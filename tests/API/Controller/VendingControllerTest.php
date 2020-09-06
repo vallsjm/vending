3950a0b8100fec18a8feef159cf64e7b2fd2fedf
tryke <valls.jm@gmail.com>
2020-09-06 19:07:56 +0200

    test buy soda

--- tests/API/Controller/VendingControllerTest.php
+++ tests/API/Controller/VendingControllerTest.php
@@ -51,4 +51,65 @@ class VengingControllerTest extends WebTestCase
         $this->assertContains('Invalid coin value', $client->getResponse()->getContent());
     }
 
+    public function testGetBuyItemSodaAction()
+    {
+        $client = static::createClient();
+
+        $payload = [
+            'price' => 1.50,
+            'amount' => 10
+        ];
+
+        $coins = [
+            '1',
+            '0.05',
+            '0.05',
+            '0.25',
+            '0.25'
+        ];
+
+        $client->request(
+            'PUT',
+            '/api/service/item/soda',
+            [],
+            [],
+            [
+                'CONTENT_TYPE' => 'application/json',
+                'ACCEPT' => 'application/xml'
+            ],
+            json_encode($payload)
+        );
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        foreach ($coins as $coin) {
+            $client->request(
+                'POST',
+                '/api/coin/insert/' . $coin,
+                [],
+                [],
+                [
+                    'CONTENT_TYPE' => 'application/json',
+                    'ACCEPT' => 'application/xml'
+                ],
+                ''
+            );
+        }
+
+        $client->request(
+            'GET',
+            '/api/item/buy/SODA',
+            [],
+            [],
+            [
+                'CONTENT_TYPE' => 'application/json',
+                'ACCEPT' => 'application/xml'
+            ],
+            ''
+        );
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('<response><item key="0">SODA</item><item key="1">0.05</item><item key="2">0.05</item></response>', $client->getResponse()->getContent());
+    }
+
 }
